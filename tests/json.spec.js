cd00678ce5f7c0f05f0965c5486e855c16b00506
Nell Hardcastle <nell@squishymedia.com>
2018-08-13 14:11:57 -0700

    Add EEG JSON sidecar test.

--- tests/json.spec.js
+++ tests/json.spec.js
@@ -32,22 +32,43 @@ describe('JSON', function(){
 		});
 	});
 
-    var meg_file = {
-        name: 'sub-01_run-01_meg.json',
-        relativePath: '/sub-01_run-01_meg.json'
+  var meg_file = {
+      name: 'sub-01_run-01_meg.json',
+      relativePath: '/sub-01_run-01_meg.json'
+  };
+
+  it('*_meg.json sidecars should have required key/value pairs', function(){
+      var jsonObj = '{"TaskName": "Audiovis", "SamplingFrequency": 1000, ' +
+                    ' "PowerLineFrequency": 50, "DewarPosition": "Upright", ' +
+                    ' "SoftwareFilters": "n/a", "DigitizedLandmarks": true,' +
+                    ' "DigitizedHeadPoints": false}';
+      validate.JSON(meg_file, jsonObj, function (issues) {
+          assert(issues.length === 0);
+      });
+
+      var jsonObjInval = jsonObj.replace(/"SamplingFrequency": 1000, /g, '');
+      validate.JSON(meg_file, jsonObjInval, function(issues){
+          assert(issues && issues.length === 1);
+      });
+  });
+
+    const eeg_file = {
+        name: 'sub-01_run-01_eeg.json',
+        relativePath: '/sub-01_run-01_eeg.json',
+        path: 'sub-01_run-01_eeg.json'
     };
 
-    it('*_meg.json sidecars should have required key/value pairs', function(){
-        var jsonObj = '{"TaskName": "Audiovis", "SamplingFrequency": 1000, ' +
-                      ' "PowerLineFrequency": 50, "DewarPosition": "Upright", ' +
-                      ' "SoftwareFilters": "n/a", "DigitizedLandmarks": true,' +
-                      ' "DigitizedHeadPoints": false}';
-        validate.JSON(meg_file, jsonObj, function (issues) {
+    it('*_eeg.json sidecars should have required key/value pairs', function () {
+        const jsonObj = '{"TaskName": "Audiovis", "Manufacturer": "TDT", ' +
+            ' "EEGChannelCount": 128, "EOGChannelCount": 2, ' +
+            ' "ECGChannelCount": 1, "EMGChannelCount": 2, ' +
+            ' "PowerLineFrequency": 50, "SamplingFrequency": 10, "EEGReference": "reference"}';
+        validate.JSON(eeg_file, jsonObj, function (issues) {
             assert(issues.length === 0);
         });
 
-        var jsonObjInval = jsonObj.replace(/"SamplingFrequency": 1000, /g, '');
-        validate.JSON(meg_file, jsonObjInval, function(issues){
+        const jsonObjInval = jsonObj.replace(/"EEGChannelCount": 128, /g, '');
+        validate.JSON(eeg_file, jsonObjInval, function (issues) {
             assert(issues && issues.length === 1);
         });
     });
